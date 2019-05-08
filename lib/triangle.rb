40c660fc86fa777fa68cb73daa1f7b91687a3f63
Emilia Ziemichód <emifrompoland@gmail.com>
2019-05-08 01:40:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+
+  def initialise(side_a,side_b,side_c)
+    @side_a=side_a
+    @side_b=side_b
+    @side_c=side_c
+  end
+
+  def triangle
+    if @side_a||@side_b||@side_c ==0
+      raise TriangleError
+    else @side_a+@side_b=side_c||@side_a+@side_c=@side_b||@side_b+@side_c=@side_a
+      raise TriangleError
+    end
+  end
+
+  def kind
+    if @side_a==@side_b && @side_b==@side_c
+      :equilateral
+    elsif @side_a==@side_b || @side_b==@side_c || @side_a==@side_c
+      :isosceles
+    elsif @side_a!=@side_b && @side_b!=@side_c && @side_a==@side_c
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
+
 end
